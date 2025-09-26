6a45704f55f162553ff32952d9b164d3459faddb
nguyenit24 <nguyenhuynh.463459@gmail.com>
2025-09-26 08:23:12 +0700

    GrapQL

---
+++ src/main/java/vn/iotstar/grapql_23110274/GrapQl23110274Application.java
@@ -0,0 +1,13 @@
+package vn.iotstar.grapql_23110274;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapQl23110274Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapQl23110274Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/iotstar/grapql_23110274/GrapQl23110274ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.grapql_23110274;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapQl23110274ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
